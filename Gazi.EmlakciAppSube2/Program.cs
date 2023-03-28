baba0fe9a1c0dd44f47267ae54e7601735b86b1d
Cihan Yetisken <cyetisken@gmail.com>
2023-03-28 11:40:43 +0300

    Constuctor Konusu Anlatıldı

--- Gazi.EmlakciAppSube2/Program.cs
+++ Gazi.EmlakciAppSube2/Program.cs
@@ -7,40 +7,69 @@ namespace Gazi.EmlakciAppSube2
     {
         static void Main(string[] args)
         {
-            var evim = new Ev();
-
-            Console.WriteLine("Oda sayısı giriniz:");
-            evim.Odasayisi = int.Parse(Console.ReadLine());
-            Console.WriteLine("Kat no giriniz:");
-            evim.katno = int.Parse(Console.ReadLine());
-            Console.WriteLine("Alan giriniz:");
-            evim.alan=int.Parse(Console.ReadLine());
-            Console.WriteLine("Semt giriniz:");
-            evim.semt=Console.ReadLine();
+            var evim = new Ev(-3, 2, "Gazi", 120);
             Console.WriteLine(evim.EvBilgileri());
 
+            var evim2 = new Ev();
+            Console.WriteLine(evim2.EvBilgileri());
+
+            var evim3 = new Ev();
+           
+            Console.WriteLine(Ev.Sayac);
         }
     }
 
     class Ev
     {
+        private static int sayac = 0;
+        public static int Sayac { get => sayac;}
+        public Ev()
+        {
+            sayac++;
+        }
+        public Ev(int odasayisi, int katno, string semt, double alan)
+        {
+            this.Odasayisi = odasayisi;
+            this.Katno = katno;
+            this.Alan = alan;
+            this.Semt = semt;
+            sayac++;
+        }
+
+        public Ev(int odasayisi, int katno, double alan)
+        {
+            this.Odasayisi = odasayisi;
+            this.Katno = katno;
+            this.Alan = alan;
+            this.Semt = "Gazi";
+            sayac++;
+        }
+
         private int odasayisi;
 
         public int Odasayisi
         {
             get { return odasayisi; }
-            set { odasayisi = value; }
+            set { odasayisi = Math.Abs(value); }
         }
 
-        public int katno;
-        public string semt;
-        public double alan;
-        public string EvBilgileri()=> $"Alan:{this.alan}\nOda Sayısı:{this.odasayisi}\nKat no:{this.katno}\nSemt:{this.semt}";       
+        public int Katno { get => katno; set => katno = value; }
+
+        private int katno;
+        private string semt;
+        public double Alan { get; set; }
+        public string Semt { get => semt; set => semt = value; }
+       
+
+        public string EvBilgileri() => $"Alan:{this.Alan}\nOda Sayısı:{this.odasayisi}\nKat no:{this.Katno}\nSemt:{this.Semt}";
     }
 }
 //Class: Nesnelerin sahip olması gereken özelliklerin ve eylemlerin tanımlandığı kod bloklarıdır. Classlar aynı zamanda bir veri tipidir. Referans tipi veri tiplerindendir.
 //private: Gizli anlamına gelir. Private üyelere sadece içinde bulunduğu class tarafından erişilebilir.
 //public: Public üyelere, içinde bulunduğu class dışından da erişilebilir.
 //static olarak tanımlanan üyelere nesne referansı ile erişilemez. Bu üyeler nesnelerin değil, class'ın bir parçası olarak tanımlanırlar.
+
+//Constructor-Yapıcı Metod-Kurucu Metod: Class içinde tanımlanmış olan field/property'lere varsayılan değerlerini atayarak onları kullanıma hazır hale getirirler.
+// Her classta parametre almayan varsayılan bir constructor bulunur.
 
 
